1cfbb81822d44a633fbf2ec96eed947701d35248
MyoLinTun <mgmyolintun@gmail.com>
2025-12-02 22:33:32 +0630

    area calculator

---
+++ main.py
@@ -0,0 +1,8 @@
+print("******* Area of a Circle Calculator ******* \n");
+
+radius = int(input("Enter the radius of a circle:")); #change the input string into integer
+Area = round(3.142*(radius)**2 , 2);   #round into 2 decimals
+
+print(f"\n Area of a circle is {Area} squared units.\n");
+
+#we can calculate areas and volumes of other shapes easily by changing the formula.
